2b7c1a2c33e166f4381f04ce58ddd83f3fdb9a7d
levap-21 <igorplatov2002@mail.ru>
2022-05-20 11:28:53 +0300

    Парсинг с сайта onhockey

---
+++ server/sources/onhockey/extractor.py
@@ -0,0 +1,34 @@
+"""Извлечение данных с onhockey.tv"""
+
+from typing import List
+
+from bs4 import BeautifulSoup
+
+from server.sources.data_requests import get_response
+from server.sources.onhockey.constants import SiteInfo
+from common.func import get_valid_link, is_vk_link, get_video_from_vk
+
+
+def get_teams(info: str) -> List[str]:
+    """Парсинг строки с названиями команд"""
+    return info.split(' - ')
+
+
+async def get_source_link(channel: str):
+    """
+    Ссылка на трансляцию
+    Args:
+        channel: внутренний URL сайта
+
+    Returns:
+        Прямая ссылка на трансляцию
+    """
+    res = await get_response(channel, SiteInfo.ADDRESS)
+    link = None
+    if res:
+        iframe = BeautifulSoup(res, 'html.parser').find('iframe')
+        url = iframe and iframe.get('src')
+        link = get_valid_link(iframe and iframe.get('src'))
+        if not link and is_vk_link(url):
+            link = get_video_from_vk(url)
+    return link
